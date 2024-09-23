75ca4c72dedbd602979d9011722408e2bcf72484
Stef Janssens <stef.j.1996@outlook.com>
2024-09-23 23:34:58 +0200

    Remove comments gui.js

--- src/gui.js
+++ src/gui.js
@@ -1,20 +1,5 @@
 const sidebarSection = $('.sidebar-section');
 
-$(() => {
-
-})
-
 $('#sidebar-button').click(() => {
-    
     sidebarSection.hasClass('collapsed') ? sidebarSection.removeClass('collapsed') : sidebarSection.addClass('collapsed')
-
-
-
-    // if(sidebarSection.hasClass('collapsed')){
-    //     sidebarSection.removeClass('collapsed')
-    // }
-    // else{
-    //     sidebarSection.addClass('collapsed')
-    // }
-
 })
