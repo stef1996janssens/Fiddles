08e0807bfffcaf2d42d882f2253c10e774baf6a4
Stef Janssens <stef.j.1996@outlook.com>
2025-11-01 00:14:21 +0100

    Added navbar + theme toggle

---
+++ Fiddles/Pages/ColorPalette.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Fiddles.Pages;
+
+public partial class ColorPalette
+{
+    public ColorPalette()
+    {
+
+    }
+}
