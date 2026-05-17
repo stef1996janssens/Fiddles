53ab47c8272ada8c1454be2092fbb6bde5f56476
Stef Janssens <stef.j.1996@outlook.com>
2026-05-17 14:54:56 +0200

    Rework home page to have tiles to navigate to pages. Added mobile layout without navbar for mobile devices.

---
+++ Fiddles/Constants/SvgIcon.cs
@@ -0,0 +1,6 @@
+﻿namespace Fiddles.Constants;
+
+public class SvgIcon
+{
+    public const string Dartsboard = "<defs>\r\n    <mask id=\"dart-mask\">\r\n      <rect width=\"24\" height=\"24\" fill=\"white\"/>\r\n      <path transform=\"translate(12,12) rotate(-45)\" fill=\"black\" stroke=\"black\" stroke-width=\"2\" stroke-linejoin=\"round\" d=\"M 0,0 L -0.8,-3 L -0.8,-10.5 L -4,-15 L 0,-13 L 4,-15 L 0.8,-10.5 L 0.8,-3 Z\"/>\r\n    </mask>\r\n  </defs>\r\n  <path fill=\"currentColor\" fill-rule=\"evenodd\" mask=\"url(#dart-mask)\" d=\"\r\n    M 12 1 A 11 11 0 1 1 11.999 1 Z\r\n    M 12 2.4 A 9.6 9.6 0 1 0 12.001 2.4 Z\r\n    M 12 5 A 7 7 0 1 1 11.999 5 Z\r\n    M 12 6.2 A 5.8 5.8 0 1 0 12.001 6.2 Z\r\n    M 12 8.8 A 3.2 3.2 0 1 1 11.999 8.8 Z\r\n    M 12 10.5 A 1.5 1.5 0 1 0 12.001 10.5 Z\r\n  \"/>\r\n  <path transform=\"translate(12,12) rotate(-45)\" fill=\"currentColor\" d=\"M 0,0 L -0.8,-3 L -0.8,-10.5 L -4,-15 L 0,-13 L 4,-15 L 0.8,-10.5 L 0.8,-3 Z\"/>";
+}
